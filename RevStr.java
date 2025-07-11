832286abb5cf05bb47974f9780597a69dad1c951
AmarRaj Yekbote <amarrajyekbote90@gmail.com>
2025-07-11 12:18:11 +0530

    Added another methods

--- RevStr.java
+++ RevStr.java
@@ -3,15 +3,27 @@ class RevStr{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         String str = sc.nextLine();
-        StringBuilder sb =  new StringBuilder(str);
+        StringBuilder sb =  new StringBuilder(str); //or i can do here StringBuilder sb =  new StringBuilder(str).reverse(); then print sb
         sb.reverse();
         System.out.println(sb);
     }
 }
-//OTHER LOGIC
+//ANOTHER LOGIC 1
 /*String str = "AMAR";
         String rev = "";
             for(int i = str.length()-1;i>=0;i--){
                 rev+=str.charAt(i);
             }
-        System.out.println(rev); */
+        System.out.println(rev); 
+        
+//ANOTHER LOGIC 2
+    StringBuilder ans =  new StringBuilder();
+    for(int i=str.length()-1;i>=0;i--){
+    ans.append(str.charAt(i));
+    }
+    System.out.println(ans);
+    
+        
+        
+        */
+
