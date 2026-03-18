c4405e9b79017ac206e994f8f275dd4d3e58c0b8
AmarRaj Yekbote <amarrajyekbote90@gmail.com>
2026-03-18 12:45:23 +0530

    Fix palindrome check logic in ispalindrome method

--- Palindrome.java
+++ Palindrome.java
@@ -3,7 +3,7 @@ class Palindrome {
         String s = String.valueOf(x);
         int n = s.length();
             for(int i=0;i<n/2;i++){
-                if(s.charAt(i) != s.charAt(n-1)) return false;
+                if(s.charAt(i) != s.charAt(n-1-i)) return false;
             }
         return true;
     }
